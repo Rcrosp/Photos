7d169cc957453bdc554c041246d6695f405ad55c
chysun <chysun@abcft.com>
2018-07-03 19:02:49 +0800

    Initial Commit

---
+++ Photos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photos
+//
+//  Created by Apple on 2018/7/3.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
